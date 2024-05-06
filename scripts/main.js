8904b986de9bfc761d3f4b0e2dbba1662a2ddcbf
kazuya yumita <93020042+kazuya90@users.noreply.github.com>
2024-05-06 10:46:18 +0900

    get tabs infomation

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,24 @@ function excecute_content_script() {
     .executeScript({ file: "/scripts/content_script.js" });
 }
 
-const f1 = document.getElementById("function1")
+function logTabs(tabs) {
+  for (let tab of tabs) {
+    // tab.url requires the `tabs` permission
+    console.log(tab.url);
+  }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+
+
+const f1 = document.getElementById("function1");
 
 f1.addEventListener("click", (e) => {
   excecute_content_script();
+  var querying = browser.tabs.query({});
+  querying.then(logTabs, onError);
 });
 
