ead921fd9797c6cf0eaca35f4fad70369a80bd56
kazuya yumita <93020042+kazuya90@users.noreply.github.com>
2024-12-08 09:14:35 +0900

    2024/12/08
    ver1.1を作成

--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,64 +1,72 @@
-//htmlの各要素を持つクラス
+
+console.log("Content script loaded");
+
+// main.jsからtabIndexを受け取る
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  var tabIndex = message.tabIndex + 1;
+  var plane_html = message.plane_html;
+  console.log("tabIndex received:", tabIndex);
+  //tabindexを二桁の0埋め文字列に変換
+  tabIndex = ("00" + tabIndex).slice(-2);
+  // メッセージを受け取った後にhtml_download関数を呼び出す
+  html_download(tabIndex, plane_html);
+});
+
+// main関数
+function html_download(tabIndex, plane_html) {
+console.log("html_download called");
+  var html = new HtmlElement(tabIndex);
+  var download = new Download(html.html, html.title);
+  var plane_download = new Download(plane_html, 'plane_' + html.title);
+  console.log(html.html);
+}
+
+// htmlの各要素を持つクラス
 class HtmlElement {
-  constructor() {
+  constructor(tabIndex) {
     this._html = document.getElementsByTagName("html")[0];
-    this.html = this._html.outerHTML;
-    this.title = this._html.getElementsByTagName("title")[0].innerHTML;
-    this._html
+    this.html = this.doctype + '\n' + this._html.outerHTML;
+    this.title = tabIndex + '_' + this._html.getElementsByTagName("title")[0].innerHTML;
+    console.log("HtmlElement created:", this.html, this.title);
+  }
+  //doctypeのタグを取得
+  get doctype() {
+    var node = document.doctype;
+    var doctype = "<!DOCTYPE "
+      + node.name
+      + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '')
+      + (!node.publicId && node.systemId ? ' SYSTEM' : '')
+      + (node.systemId ? ' "' + node.systemId + '"' : '')
+      + '>';
+    return doctype;
   }
 }
 
-//画面キャプチャクラス
-class Capture {
-  constructor() {
-    this.canvas = document.createElement("canvas");
-    this.setAttributeCanvas();
-    this.ctx = this.canvas.getContext("2d");
-    this.ctx.drawWindow(window._content, 0, 0, width, height, "rgb(0,0,0)");
-    this.appendpage();
-  }
-
-  setAttributeCanvas = () => {
-    this.canvas.setAttribute("height", window._content.document.height);
-    this.canvas.setAttribute("width", window._content.document.width);
-  }
-  appendpage = () => {
-    body = document.getElementsByTagName("body")[0];
-    body.append("test");
-    console.log(this.canvas);
-  }
-
-}
-
-//ダウンロードに関するクラス
+// ダウンロードに関するクラス
 class Download {
   constructor(_download_text, _download_file_name) {
-    //テキストファイルとしてダウンロードするテキストの中身
+    // テキストファイルとしてダウンロードするテキストの中身
     this.text = _download_text;
-    //ダウンロードするテキストファイルの名前
+    // ダウンロードするテキストファイルの名前
     this.download_file_name = _download_file_name;
+    console.log("Download created:", this.text, this.download_file_name);
     this.download();
   }
 
-  download = (text) => {
+  download = () => {
     var blob = new Blob([this.text], { type: 'text/plain' });
     var url = URL.createObjectURL(blob);
     // ダウンロードリンクを作成
     var a = document.createElement('a');
     a.href = url;
-    a.download = this.download_file_name + '.txt';
+    a.download = this.download_file_name + '.html';
     a.style.display = 'none';
     document.body.appendChild(a);
     // リンクをクリックしてダウンロードを開始
     a.click();
+    document.body.removeChild(a);
+    console.log("Download initiated");
   }
 }
 
 
-//DOM構築のため５秒待機
-function main() {
-  var html = new HtmlElement();
-  var download = new Download(html.html, html.title);
-  var capture = new Capture();
-}
-setTimeout(main, 5000);
--- scripts/main.js
+++ scripts/main.js
@@ -1,67 +1,33 @@
 //アクティブなタブで実行するスクリプト
 function excecute_content_script() {
-  browser.tabs
-    .executeScript({ file: "/scripts/content_script.js" });
+  browser.tabs.executeScript({ file: "/scripts/content_script.js" });
 }
-
-//画像をダウンロード
-function downloadScreenshot(dataUri, title) {
-  let link = document.createElement('a');
-  link.href = dataUri;
-  link.download = title + '.png';
-  link.click();
-}
-
 
 //ツールバーに表示されたボタンを取得
 const dom_button = document.getElementById("dom_button");
 
 //ボタンクリック時すべてのタブでcontent_script.jsを実行
 dom_button.addEventListener("click", (e) => {
-  // excecute_content_script();
-  var querying = browser.tabs.query({}, tabs => {
-    for (let tab of tabs) {
-      //コンテンツスクリプトを実行
-      browser.tabs.update(tab.id, { active: true }, excecute_content_script("./content_script.js"));
-    }
-  });
-});
-
-//画面キャプチャ
-const capture_button = document.getElementById("capture_button");
-capture_button.addEventListener("click", (e) => {
-  // excecute_content_script();
-  var querying = browser.tabs.query({}, tabs => {
-    for (let tab of tabs) {
-
-      //画面をキャプチャしてダウンロード
-      //高さを取得する
+  browser.tabs.query({}, tabs => {
+    tabs.forEach((tab, tabIndex) => {
+      // コンテンツスクリプトを実行
       browser.tabs.executeScript(tab.id, {
-        code: 'document.documentElement.scrollHeight'
-      }).then(results => {
-        let fullHeight = results[0];
-        //キャプチャを実行
-        browser.tabs.captureTab(
-          tab.id, {
-          format: 'png',
-          rect: { x: 0, y: 0, width: window.screen.width, height: fullHeight }
-        }).then(imageUri => {
-          // スクリーンショットの画像URIを取得
-          downloadScreenshot(imageUri, tab.title);
-        });
+        file: "/scripts/content_script.js"
+      }, () => {
+        //アクティブなタブのhtmlを取得
+        var plane_html = gethtml(tab.url);
+        // tabIndexをcontent_script.jsにメッセージとして送信
+        browser.tabs.sendMessage(tab.id, { tabIndex: tabIndex, plane_html: plane_html });
       });
-    }
+    });
   });
 });
 
-//ストレージテスト
-function gotMonster(item) {
-  console.log(`${item.test}`);
-  console.log(`${item.name}`);
+
+//アクティブなタブのhtmlを取得
+function gethtml(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.send();
+  return xhr.responseText;
 }
-
-var test = { "test": "test", "name": "taro" };
-
-browser.storage.local.set(test)
-
-browser.storage.local.get(test).then(gotMonster)
